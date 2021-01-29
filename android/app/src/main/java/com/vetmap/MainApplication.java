d2fc418d4099ef34a1d5507581603743b1bc9e33
Avais392 <avais392@gmail.com>
2021-01-30 00:41:45 +0500

    ,

--- android/app/src/main/java/com/vetmap/MainApplication.java
+++ android/app/src/main/java/com/vetmap/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import fr.greweb.reactnativeviewshot.RNViewShotPackage;
 import com.reactnativecommunity.rnpermissions.RNPermissionsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactInstanceManager;
